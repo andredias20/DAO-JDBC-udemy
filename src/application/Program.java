899698ac230ffd707761d32fa5177f1bbfa33f4c
andredias20 <andre.c.dias2000@gmail.com>
2022-02-09 10:00:59 -0300

    Seller entitie class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		
 		Department dp = new Department(1, "Books");
 		
-		System.out.println(dp);
+		
+		
+		Seller sl = new Seller(21, "Carlinhos", "carlito@gmail.com", new Date(), 2000.00, dp);
+		
+		System.out.println(sl);
 		
 	}
 }
