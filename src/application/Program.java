60c6610bfc81b94f967f6ffa16381fbb0127a6bc
andredias20 <andre.c.dias2000@gmail.com>
2022-02-09 12:39:42 -0300

    implementing findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,26 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		
-		Department dp = new Department(1, "Books");
-		
-		
-		
-		Seller sl = new Seller(21, "Carlinhos", "carlito@gmail.com", new Date(), 2000.00, dp);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Seller seller = sellerDao.findById(3);
 		
-		
-		System.out.println(sl);
+		System.out.println(seller);
 		
 	}
 }
