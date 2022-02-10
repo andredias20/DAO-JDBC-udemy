f65bcf59b7c294999c0552edf8d140673bff6d10
andredias20 <andre.c.dias2000@gmail.com>
2022-02-10 00:45:56 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,8 +34,16 @@ public class Program {
 		
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
 		sellerDao.insert(newSeller);
-
+		
 		System.out.println("Inserted! New id = "+ newSeller.getId());
+		
+		System.out.println("\n==== TEST 5: seller Update ====");
+		
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wane");
+		sellerDao.update(seller);
+
+		System.out.println("Updated!");
 		
 	}
 }
